b24130b13a2442b9b0ae9a37de1ed4096a8f14a8
xander1235 <rathodveerender25@gmail.com>
2018-07-04 14:28:54 +0530

    completed

---
+++ inOrder.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<cstdlib>
+#include<stack>
+#include<queue>
+
+using namespace std;
+
+struct node{
+  int data;
+  struct node *left, *right;
+};
+
+struct node* bst(struct node *root,int data){
+  if(!root){
+    root = (struct node*)malloc(sizeof(struct node));
+    if(!root){
+      cout <<"memory not alloted" <<endl;
+    }
+    else{
+      root->data = data;
+      root->left = NULL;
+      root->right = NULL;
+    }
+  }
+  else{
+    if(root->data > data){
+      root->left = bst(root->left,data);
+    }
+    else if(root->data < data){
+      root->right = bst(root->right,data);
+    }
+  }
+  return root;
+}
+
+void preorder(struct node *root){
+  if(!root){
+    return;
+  }
+  else{
+    preorder(root->left);
+    cout <<root->data <<" ";
+    preorder(root->right);
+  }
+}
+
+int main(){
+  int n,i,j;
+  cin >>n;
+  struct node *root=NULL;
+  for(i=0;i<n;i++){
+    cin >>j;
+    root = bst(root,j);
+  }
+  preorder(root);
+  //levelOrder(root);
+  cout <<endl;
+  return 0;
+}
